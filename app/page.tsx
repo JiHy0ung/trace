c44a152f8cab187ae4b6ab372a6c03606df14ba1
JiHyoung You <fbwlgud51@gmail.com>
2026-06-17 22:04:40 +0900

    chore: 불필요 파일 및 코드 제거

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,6 @@ export default async function Home() {
     ? data?.hdurl
     : "https://apod.nasa.gov/apod/image/2606/SaturnRingsMoons_Cassini_960.jpg";
 
-  // 기본 이미지 링크
-  // https://apod.nasa.gov/apod/image/2606/SaturnRingsMoons_Cassini_960.jpg
   return (
     <div
       className="w-full h-screen bg-cover bg-center"
