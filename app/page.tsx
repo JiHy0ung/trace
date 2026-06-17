86023b3cb9c8a087d7165b7cd9c52559dcaf3cea
JiHyoung You <fbwlgud51@gmail.com>
2026-06-17 21:32:50 +0900

    chore: test 링크 삭젝

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 export default async function Home() {
   const data = await getAPOD();
 
+  // 기본 이미지 링크
+  // https://apod.nasa.gov/apod/image/2606/SaturnRingsMoons_Cassini_960.jpg
   return (
     <div
       className="w-full h-screen bg-cover bg-center"
-      style={{ backgroundImage: `url(${data.hdurl})` }}
-    >
-      <Link href="/til">TIL 페이지 이동</Link>
-    </div>
+      style={{ backgroundImage: `url(${data?.hdurl})` }}
+    ></div>
   );
 }
