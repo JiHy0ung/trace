8cfa8bdc078c5e0e42e4a66be2ce93bb43f216ac
JiHyoung You <fbwlgud51@gmail.com>
2026-06-17 21:33:44 +0900

    style: til 페이지, til 카드 다크 모드 UI 적용

--- app/til/page.tsx
+++ app/til/page.tsx
@@ -1,17 +1,25 @@
 import { TilCard } from "@/components/TilCard";
 import { dummyData } from "@/lib/dummy";
+import Link from "next/link";
 
 export default function TilPage() {
   const data = dummyData;
   return (
     <div
       className="w-full h-screen
-      flex flex-col justify-start items-center"
+      flex flex-col justify-start items-center
+      pt-12"
     >
       <div className="w-full max-w-2xl flex flex-col gap-9 px-6">
         <h1
-          className="text-6xl font-normal tracking-tighter text-indigo-200/70
-          [&_strong]:text-white [&_strong]:text-5xl [&_strong]:font-black"
+          className="
+          font-normal tracking-tighter
+          text-6xl
+          text-zinc-500 dark:text-zinc-400
+          [&_strong]:text-zinc-700
+          [&_strong]:dark:text-zinc-100
+          [&_strong]:text-5xl
+          [&_strong]:font-black"
         >
           <strong>T</strong>oday <strong>I</strong> <strong>L</strong>earn
         </h1>
@@ -22,7 +30,9 @@ export default function TilPage() {
             gap-5"
         >
           {data.map((item) => (
-            <TilCard key={item.id} data={item} />
+            <Link key={item.id} href={`/til/${item.id}`}>
+              <TilCard data={item} />
+            </Link>
           ))}
         </div>
       </div>
--- components/TilCard.tsx
+++ components/TilCard.tsx
@@ -9,8 +9,8 @@ export const TilCard = ({ data }: TilCardProps) => {
     <div
       className="
       flex flex-col justify-center items-start gap-2 px-6 py-5
-      bg-indigo-900/10
-      border-[1px] border-indigo-300/30 rounded-xl"
+      bg-zinc-200/50 dark:bg-zinc-900
+      border-zinc-400/30 dark:border-zinc-300/30 border-[1px] rounded-xl"
     >
       <div className="flex justify-center items-center gap-2">
         {data.tags.map((tag, index) => (
@@ -19,8 +19,8 @@ export const TilCard = ({ data }: TilCardProps) => {
             className="
             flex justify-center items-center px-3
             rounded-xl
-            text-indigo-300 font-medium text-sm
-            bg-indigo-300/10
+            text-zinc-100 dark:text-zinc-200 font-medium text-sm
+            bg-zinc-400 dark:bg-zinc-700
             "
           >
             {tag}
@@ -30,7 +30,11 @@ export const TilCard = ({ data }: TilCardProps) => {
 
       <h1 className="font-semibold">{data.title}</h1>
 
-      <p className="text-sm text-white/60 break-keep">
+      <p
+        className="
+      text-sm text-zinc-500 dark:text-zinc-400
+      break-keep"
+      >
         {data.contents.length > 100
           ? data.contents.slice(0, 100) + "..."
           : data.contents}
@@ -42,7 +46,7 @@ export const TilCard = ({ data }: TilCardProps) => {
           className="
           relative
           inline-block
-          text-indigo-300
+          text-zinc-600 dark:text-zinc-300
           cursor-pointer
 
           after:absolute
